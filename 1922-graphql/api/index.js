5cdccbd7a8070f455b7e50083f104fa194342f8b
fabrizioabreu <fabrizioabreu@live.com>
2023-05-23 21:30:33 -0300

    Os schemas do GraphQL

--- 1922-graphql/api/index.js
+++ 1922-graphql/api/index.js
@@ -1,1 +1,22 @@
-const { ApolloServer} = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server")
+
+const users = [
+  {
+    nome: "Ana",
+    ativo: true
+  },
+  {
+    nome: "Marcia",
+    ativo: false
+  }
+]
+
+const typeDefs = gql `
+  type User {
+    nome: String!
+    ativo: Boolean!
+    email: String
+  }
+`
+
+const server = new ApolloServer({ typeDefs, nodeEnv })
