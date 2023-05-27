30e7d10620c1a8799e3888164fce7414d1256665
fabrizioabreu <fabrizioabreu@live.com>
2023-05-27 19:18:56 -0300

    Schema de matrículas

--- 1922-graphql/api/user/datasource/user.js
+++ 1922-graphql/api/user/datasource/user.js
@@ -32,8 +32,11 @@ class UsersAPI extends RESTDataSource{
   async adicionaUser(user) {
     const users = await this.get('/users')
     user.id = users.length +1
+
     const role = await this.get(`roles?type=${user.role}`)
+
     await this.post('users', {...user,role: role[0].id})
+    
     return ({
       ...user,
       role: role[0]
