c099bc722dad056730c250fe0764fb3b06663d8a
fabrizioabreu <fabrizioabreu@live.com>
2023-05-27 18:11:15 -0300

    CRUD de Turmas

--- 1922-graphql/api/turma/datasource/turma.js
+++ 1922-graphql/api/turma/datasource/turma.js
@@ -11,6 +11,50 @@ class TurmasAPI extends SQLDataSource {
       .from('turmas')
   }
 
+  async getTurma(id) {
+    const turma = await this.db
+      .select('*')
+      .from('turmas')
+      .where({ id: Number(id)})
+    return turma[0]
+  }
+
+  async incluiTurma(novaTurma) {
+    const novaTurmaId = await this.db
+      .insert(novaTurma)
+      .returning('id')
+      .into('turmas')
+    
+    const turmaInserida = await this.getTurma(novaTurmaId[0])
+
+    return ({ turmaInserida })
+  }
+
+  async atualizaTurma(novosDados) {
+    await this.db
+      .update({ ...novosDados.turma })
+      .where({ id: Number(novosDados.id) })
+      .into('turmas')
+ 
+    const turmaAtualizada = await this.getTurma(novosDados.id)
+    return ({
+      ...turmaAtualizada
+    })
+  }
+
+  async deletaTurma(id) {
+    await this.db('turmas')
+      .where({ id: id })
+      .del()
+ 
+    this.Resposta.mensagem = "registro deletado"
+
+    console.log('Resposta 1 ', this.Resposta.mensagem)
+    console.log('Resposta 2 ', this.Resposta)
+
+    return this.Resposta
+  }
+
 }
 
 module.exports = TurmasAPI
