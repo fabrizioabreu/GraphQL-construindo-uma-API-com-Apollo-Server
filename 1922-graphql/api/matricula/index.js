3dc4d8a9bd87f4b770f5bdee383a6c6b3e50b4d1
fabrizioabreu <fabrizioabreu@live.com>
2023-05-27 19:35:51 -0300

    Organizando o projeto

---
+++ 1922-graphql/api/matricula/index.js
@@ -0,0 +1,7 @@
+const matriculaSchema = require('./schema/matricula.graphql')
+// const matriculaResolvers = require('./resolvers/matriculaResolver')
+// const matriculasAPI = require('./datasource/matricula')
+
+module.exports = {
+  matriculaSchema,
+}
